45abc8b6f5033661dc5b0c473e4587af9d629288
Asfamaghfiratunnisa <80738487+Asfamaghfiratunnisa@users.noreply.github.com>
2021-04-10 18:20:55 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+package Invenstory;
+
+
+public class Invenstori {
+
+    public static void main(String[] args) {
+       Barang in = new Barang();
+       in.menu();
+       in.getNilaiData();
+    }
+    
+}
